0ed75c4016e115e3aa62eef1694e9ad2f0c69662
sliemansalah <slieman2001@hotmail.com>
2025-11-22 11:44:03 +0200

    ..

--- quran-app/app/Models/Surah.php
+++ quran-app/app/Models/Surah.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Surah extends Model
 {
@@ -17,6 +18,8 @@ class Surah extends Model
         'description'
     ];
 
+    protected $appends = ['name_arabic', 'name_english', 'verses_count'];
+
     public function ayahs(): HasMany
     {
         return $this->hasMany(Ayah::class);
@@ -25,5 +28,27 @@ class Surah extends Model
     public function userHifz(): HasMany
     {
         return $this->hasMany(UserHifz::class);
+    }
+
+    // Accessors for compatibility with frontend
+    protected function nameArabic(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->name,
+        );
+    }
+
+    protected function nameEnglish(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->name_en,
+        );
+    }
+
+    protected function versesCount(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->ayah_count,
+        );
     }
 }
